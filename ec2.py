74710d6374e184fb60fc19a73f582a0c821d3e54
RobertoGil <roberto.gil021@comunidadunir.net>
2021-06-24 07:56:21 +0000

    version 1.0

--- ec2.py
+++ ec2.py
@@ -1,39 +1,37 @@
 import boto3
 import sys
+import time
 
 
 ec2 = boto3.client('ec2')
 
 action = sys.argv[1] 
 instances = [sys.argv[2]] if len(sys.argv) > 2 else '.'
-#region = sys.argv[3]
 
 def main():
-    
-        
     if action == "stop":
-                
         stopInstance()
-        
     elif action == "start":
-        
         startInstance()
-        
     elif action == "list":
-    
         listInstances()
-        
     else:
-        print("Choose start, stop or list")
+        print("Choose start + id, stop + or list")
         
         
 def startInstance():
     ec2.start_instances(InstanceIds=instances)
-    print('starting instance...')
+    print("============================")
+    print('Try to start Ec2 instance...')
+    print("============================")
+    time.sleep(30)
     
 def stopInstance():
     ec2.stop_instances(InstanceIds=instances)
-    print('stopping instance...')
+    print("===========================")
+    print('Try to stop Ec2 instance...')
+    print("===========================")
+    time.sleep(30)
     
 def listInstances():
     response = ec2.describe_instances()
@@ -41,7 +39,8 @@ def listInstances():
         for instance in reservation["Instances"]:
             print(instance["InstanceId"])
             print(instance["State"])
-            
+            print("====================")
+       
     
 if __name__ == '__main__':
     main()     
